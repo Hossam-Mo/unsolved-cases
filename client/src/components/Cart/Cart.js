f71f42455ed92582c2616496260606f457b8395c
HossamUK <hossam.m.pro@gmail.com>
2022-02-02 19:37:42 +0200

    cart

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,9 +1,26 @@
 import React from "react";
 import Refrance from "../Refrance/Refrance";
 import "./cart.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 export default function Cart() {
   const arr = [
+    {
+      name: "جريمة قتل في يوم الزفاف",
+      id: 1,
+      images: [
+        "https://cdn.salla.sa/lwWeD/q6L6AjhMGrdbGs2yXBB2MekzOCNfr5LyQdciUhmJ.jpg",
+        "https://cdn.salla.sa/lwWeD/ICmBBoSzZGKSEkGxohsfNLhE5BUO8bPNKdCBqiKj.jpg",
+        "https://cdn.salla.sa/lwWeD/66qZ934Cr44jWDdTnwoCwPMR0Y4NIDMuaHpxOkj8.png",
+      ],
+      price: 25,
+      priceDes: "السعر شامل الضريبه",
+      description:
+        "قضيه لم تحل منذ اكثر من عشرين سنه تم تلفيق مقتل غاده ماسي على ابو جميل مهمتك كـ محقق حل غموض اللغز وكشف الجاني الحقيقي",
+      ageRating: "14",
+      difficulty: "7",
+      counter: 1,
+    },
     {
       name: "جريمة قتل في يوم الزفاف",
       id: 1,
@@ -30,6 +47,9 @@ export default function Cart() {
           {arr.map((item) => {
             return (
               <div className="cart_item">
+                <div className="cart_itemDelete">
+                  <AiOutlineCloseCircle></AiOutlineCloseCircle>
+                </div>
                 <div className="cart_itemDesc">
                   <div className="cart_itemImage">
                     <img src={item.images[0]} alt={item.name}></img>
